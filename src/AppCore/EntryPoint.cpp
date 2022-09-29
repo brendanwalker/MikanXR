8aa3ef7b99cbe839f3567e7a68ff9eed572532e4
Brendan Walker <brendan@millerwalker.net>
2022-09-28 22:17:23 -0700

    Adding new win32 app icon

--- src/AppCore/EntryPoint.cpp
+++ src/AppCore/EntryPoint.cpp
@@ -2,9 +2,22 @@
 #include "App.h"
 
 //-- entry point -----
+#ifdef WIN32
+int __stdcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, 
+	int nShowCmd)
+{
+	App app;
+
+	return app.exec(__argc, __argv);
+}
+#else
 extern "C" int main(int argc, char* argv[])
 {
 	App app;
 
 	return app.exec(argc, argv);
 }
+#endif
