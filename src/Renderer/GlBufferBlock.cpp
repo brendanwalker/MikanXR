1f569bffad3912b78c34fb94b6e93bd4ca10a29d
Brendan Walker <brendan@millerwalker.net>
2023-11-18 00:20:04 -0800

    More porting over GlBufferBlock class

---
+++ src/Renderer/GlBufferBlock.cpp
@@ -0,0 +1,92 @@
+#include "GlBufferBlock.h"
+
+#include <GL/glew.h>
+
+GlBufferBlock::GlBufferBlock() :
+	m_Name(""),
+	m_Binding(-1),
+	m_Size(0)
+{}
+
+GlBufferBlock::GlBufferBlock(const std::string& name, int binding) :
+	m_Name(name),
+	m_Binding(binding),
+	m_Size(0)
+{}
+
+GlBufferBlock::~GlBufferBlock()
+{}
+
+void GlBufferBlock::SetName(const std::string& name)
+{
+	m_Name = name;
+}
+
+std::string GlBufferBlock::GetName()
+{
+	return m_Name;
+}
+
+const std::vector<GlShaderVar>& GlBufferBlock::GetVars() const
+{
+	return m_Vars;
+}
+
+void GlBufferBlock::SetBinding(int binding)
+{
+	m_Binding = binding;
+}
+
+int GlBufferBlock::GetBinding()
+{
+	return m_Binding;
+}
+
+int GlBufferBlock::size()
+{
+	return m_Size;
+}
+
+void GlBufferBlock::AddVar(GlShaderVar var)
+{
+	std::string name = var.GetName();
+	var.SetName(name.substr(name.find_first_of(".") + 1).c_str());
+	m_Vars.push_back(var);
+
+	switch (var.GetType())
+	{
+		case GL_BOOL:
+		case GL_INT:
+		case GL_UNSIGNED_INT:
+			m_Size += sizeof(int);
+			break;
+		case GL_BOOL_VEC2:
+		case GL_INT_VEC2:
+		case GL_UNSIGNED_INT_VEC2:
+			m_Size += sizeof(int) * 2;
+			break;
+		case GL_BOOL_VEC3:
+		case GL_INT_VEC3:
+		case GL_UNSIGNED_INT_VEC3:
+			m_Size += sizeof(int) * 3;
+			break;
+		case GL_BOOL_VEC4:
+		case GL_INT_VEC4:
+		case GL_UNSIGNED_INT_VEC4:
+			m_Size += sizeof(int) * 4;
+			break;
+
+		case GL_FLOAT:
+			m_Size += sizeof(float);
+			break;
+		case GL_FLOAT_VEC2:
+			m_Size += sizeof(float) * 2;
+			break;
+		case GL_FLOAT_VEC3:
+			m_Size += sizeof(float) * 3;
+			break;
+		case GL_FLOAT_VEC4:
+			m_Size += sizeof(float) * 4;
+			break;
+	}
+}
---
+++ src/Renderer/GlBufferBlock.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "GlTypesFwd.h"
+#include "GlShaderVar.h"
+
+#include <vector>
+
+class GlBufferBlock
+{
+public:
+	GlBufferBlock();
+	GlBufferBlock(const std::string& name, int binding);
+	~GlBufferBlock();
+
+	void SetName(const std::string& name);
+	std::string GetName();
+	const std::vector<GlShaderVar>& GetVars() const;
+	void SetBinding(int binding);
+	int GetBinding();
+	int size();
+
+	void AddVar(GlShaderVar var);
+
+private:
+	std::string m_Name;
+	std::vector<GlShaderVar> m_Vars;
+	int m_Binding;
+	int m_Size;
+};
