a75e0852ee39d34f10627617b48e206c06b9a0ea
Brendan Walker <brendan@millerwalker.net>
2023-12-09 02:48:07 -0800

    Add node factories to CompositorNodeGraph

--- src/NodeEditors/Graphs/CompositorNodeGraph.cpp
+++ src/NodeEditors/Graphs/CompositorNodeGraph.cpp
@@ -1,4 +1,8 @@
 #include "CompositorNodeGraph.h"
+#include "Nodes/DrawTriMeshNode.h"
+#include "Nodes/EventNode.h"
+#include "Nodes/MousePosNode.h"
+#include "Nodes/TextureNode.h"
 #include "Nodes/TimeNode.h"
 
 CompositorNodeGraph::CompositorNodeGraph() : NodeGraph()
@@ -6,5 +10,9 @@ CompositorNodeGraph::CompositorNodeGraph() : NodeGraph()
 	NodeGraphPtr ownerGraph= shared_from_this();
 
 	// Nodes this graph can spawn
+	m_nodeFactories.push_back(NodeFactory::create<DrawTriMeshNodeFactory>(ownerGraph));
+	m_nodeFactories.push_back(NodeFactory::create<EventNodeFactory>(ownerGraph));
+	m_nodeFactories.push_back(NodeFactory::create<MousePosNodeFactory>(ownerGraph));
+	m_nodeFactories.push_back(NodeFactory::create<TextureNodeFactory>(ownerGraph));
 	m_nodeFactories.push_back(NodeFactory::create<TimeNodeFactory>(ownerGraph));
 }
