ed6bf9b4b356b66b3c731165d2745073dca2cfa8
Brendan Walker <brendan@millerwalker.net>
2023-12-24 21:59:42 -0800

    Handle empty name in EventNode::editorGetTitle

--- src/NodeEditors/Nodes/EventNode.cpp
+++ src/NodeEditors/Nodes/EventNode.cpp
@@ -69,7 +69,7 @@ void EventNode::editorRenderPushNodeStyle(const NodeEditorState& editorState) co
 
 std::string EventNode::editorGetTitle() const
 {
-	return m_eventName;
+	return m_eventName.empty() ? m_eventName : k_nodeClassName;
 }
 
 // -- EventNode Factory -----
