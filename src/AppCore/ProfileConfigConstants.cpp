b97611bc7de643539d26763d0568794fcbc07c17
Brendan Walker <brendan@millerwalker.net>
2023-12-26 01:42:01 -0800

    Exposing stencil string constants

--- src/AppCore/ProfileConfigConstants.cpp
+++ src/AppCore/ProfileConfigConstants.cpp
@@ -7,9 +7,16 @@ const std::string g_patternTypeStrings[(int)eCalibrationPatternType::COUNT] = {
 };
 const std::string* k_patternTypeStrings = g_patternTypeStrings;
 
-const std::string g_stencilTypeStrings[(int)eStencilType::COUNT] = {
+
+const char* g_szStencilTypeStrings[(int)eStencilType::COUNT] = {
 	"quad",
 	"box",
 	"model",
 };
-extern const std::string* k_stencilTypeStrings = g_stencilTypeStrings;
+const std::string g_stencilTypeStrings[(int)eStencilType::COUNT] = {
+	g_szStencilTypeStrings[0],
+	g_szStencilTypeStrings[1],
+	g_szStencilTypeStrings[2],
+};
+extern const char** k_szStencilTypeStrings= g_szStencilTypeStrings;
+const std::string* k_stencilTypeStrings = g_stencilTypeStrings;
