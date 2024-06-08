fc0806d041de7188294bc10ac895376e49528a9c
Brendan Walker <brendan@millerwalker.net>
2024-06-07 21:33:50 -0700

    Fixing up camera to tracker xform calibration with charuco mat

--- src/Calibration/MonoLensTrackerPoseCalibrator.cpp
+++ src/Calibration/MonoLensTrackerPoseCalibrator.cpp
@@ -27,9 +27,10 @@ struct MonoLensTrackerCalibrationState
 	int desiredSampleCount;
 
 	// Computed every frame
-	glm::dmat4 patternXform;
-	glm::dmat4 cameraXform;
+	glm::dmat4 patternXform_VRSpace;
+	glm::dmat4 cameraXform_VRSpace;
 	glm::dmat4 cameraToPatternXform;
+	glm::dmat4 cameraToMatPuckXform;
 	glm::dmat4 cameraToCameraPuckXform;
 	glm::dmat4 patternToMatPuckXform;
 	bool hasValidCapture;
@@ -61,8 +62,8 @@ struct MonoLensTrackerCalibrationState
 	void resetCalibration()
 	{
 		// Reset the capture state
-		patternXform = glm::dmat4(1.0);
-		cameraXform = glm::dmat4(1.0);
+		patternXform_VRSpace = glm::dmat4(1.0);
+		cameraXform_VRSpace = glm::dmat4(1.0);
 		cameraToPatternXform = glm::dmat4(1.0);
 		cameraToCameraPuckXform = glm::dmat4(1.0);
 		patternToMatPuckXform = glm::dmat4(1.0);
@@ -138,8 +139,8 @@ bool MonoLensTrackerPoseCalibrator::computeCameraToPuckXform()
 	}
 
 	// Fetch the calibration poses from the devices
-	const glm::dmat4 cameraPuckXform= glm::dmat4(m_cameraTrackingPuckView->getCalibrationPose());
-	const glm::dmat4 matPuckXform = glm::dmat4(m_matTrackingPuckView->getCalibrationPose());
+	const glm::dmat4 cameraPuckXform_VRSpace= glm::dmat4(m_cameraTrackingPuckView->getCalibrationPose());
+	const glm::dmat4 matPuckXform_VRSpace = glm::dmat4(m_matTrackingPuckView->getCalibrationPose());
 
 	// Look for the calibration pattern in the latest video frame
 	glm::dmat4 cameraToPatternXform;
@@ -167,27 +168,31 @@ bool MonoLensTrackerPoseCalibrator::computeCameraToPuckXform()
 		glm::translate(
 			glm::dmat4(1.0),
 			glm::dvec3(horizOffset, depthOffset, vertOffset));
-	const glm::dmat4 matPuckToPatternXform = 
-		translateToPatternXform * puckYawRot90;
+	const glm::dmat4 matPuckToPatternXform = glm_composite_xform(puckYawRot90, translateToPatternXform);
+
+	// Compute the transform from the camera to the mat puck
+	const glm::dmat4 patternToMatPuckXform = glm::inverse(matPuckToPatternXform);
+	const glm::dmat4 cameraToMatPuckXform= glm_composite_xform(patternToMatPuckXform, cameraToPatternXform);
 
 	// Compute the pattern transform in VR tracking space
-	const glm::dmat4 patternXform = matPuckXform * matPuckToPatternXform;
+	const glm::dmat4 patternXform_VRSpace = glm_composite_xform(matPuckToPatternXform, matPuckXform_VRSpace);
 
 	// Then compute the camera transform in VR tracking space
 	// by applying the inverse of the cameraToPatternXform 
 	// computed optically by OpenCV
 	const glm::dmat4 patternToCameraXform = glm::inverse(cameraToPatternXform);
-	const glm::dmat4 cameraXform = patternXform * patternToCameraXform;
+	const glm::dmat4 cameraXform_VRSpace = glm_composite_xform(patternToCameraXform, patternXform_VRSpace);
 
 	// Finally compute the transform to go from the camera tracking puck to the camera 
 	// (i.e. the relative offset of the camera from camera tracking puck)
-	const glm::dmat4 invCameraPuckXform = glm::inverse(cameraPuckXform);
-	const glm::dmat4 cameraToCameraPuckXform = invCameraPuckXform * cameraXform;
+	const glm::dmat4 invCameraPuckXform = glm::inverse(cameraPuckXform_VRSpace);
+	const glm::dmat4 cameraToCameraPuckXform = glm_composite_xform(cameraXform_VRSpace, invCameraPuckXform);
 
 	// Save the the last computed transform to the calibration state
-	m_calibrationState->patternXform = patternXform;
-	m_calibrationState->cameraXform = cameraXform;
+	m_calibrationState->patternXform_VRSpace = patternXform_VRSpace;
+	m_calibrationState->cameraXform_VRSpace = cameraXform_VRSpace;
 	m_calibrationState->cameraToPatternXform= cameraToPatternXform;
+	m_calibrationState->cameraToMatPuckXform= cameraToMatPuckXform;
 	m_calibrationState->cameraToCameraPuckXform = cameraToCameraPuckXform;
 	m_calibrationState->patternToMatPuckXform = glm::inverse(matPuckToPatternXform);
 	m_calibrationState->hasValidCapture= true;
@@ -245,14 +250,14 @@ bool MonoLensTrackerPoseCalibrator::computeCalibratedCameraTrackerOffset(
 void MonoLensTrackerPoseCalibrator::renderCameraSpaceCalibrationState()
 {
 	// Draw the most recently capture chessboard in camera space
-	//m_patternFinder->renderCalibrationPattern2D();
+	m_patternFinder->renderCalibrationPattern2D();
 
 	// Draw the camera relative transforms of the pattern (computed from solvePnP)
 	// and the mat puck location offset from the pattern origin
 	if (m_calibrationState->hasValidCapture)
 	{
-		const glm::mat4 patternXform = glm::mat4(m_calibrationState->cameraToPatternXform);
-		//const glm::mat4 patternToMatPuckXform = glm::mat4(m_calibrationState->patternToMatPuckXform);
+		const glm::mat4 cameraToPatternXform = glm::mat4(m_calibrationState->cameraToPatternXform);
+		const glm::mat4 cameraToMatPuckXform = glm::mat4(m_calibrationState->cameraToMatPuckXform);
 
 		// Compute the mat puck location relative to the mat transform we computed
 		//ProfileConfigConstPtr config = m_calibrationState->profileConfig;
@@ -264,23 +269,21 @@ void MonoLensTrackerPoseCalibrator::renderCameraSpaceCalibrationState()
 		//		glm::mat4(1.0),
 		//		glm::vec3(xOffset, yOffset, zOffset));
 		//const glm::mat4 matPuckXForm = patternXform * matPuckOffsetXform;
-		//const glm::mat4 matPuckXForm = glm_composite_xform(patternXform, patternToMatPuckXform);
+		m_patternFinder->renderSolvePnPPattern3D(cameraToPatternXform);
 
-		m_patternFinder->renderSolvePnPPattern3D(patternXform);
-
-		drawTransformedAxes(patternXform, 0.1f);
-		//drawTransformedAxes(matPuckXForm, 0.1f);
+		drawTransformedAxes(cameraToPatternXform, 0.1f);
+		drawTransformedAxes(cameraToMatPuckXform, 0.1f);
 
 		TextStyle style = getDefaultTextStyle();
-		drawTextAtWorldPosition(style, glm_mat4_get_position(patternXform), L"Mat");
-		//drawTextAtWorldPosition(style, glm_mat4_get_position(matPuckXForm), L"Puck");
+		drawTextAtWorldPosition(style, glm_mat4_get_position(cameraToPatternXform), L"Mat");
+		drawTextAtWorldPosition(style, glm_mat4_get_position(cameraToMatPuckXform), L"Puck");
 	}
 }
 
 void MonoLensTrackerPoseCalibrator::renderVRSpaceCalibrationState()
 {
 	// Draw the most recently captured chessboard projected into VR
-	m_patternFinder->renderSolvePnPPattern3D(m_calibrationState->patternXform);
+	m_patternFinder->renderSolvePnPPattern3D(m_calibrationState->patternXform_VRSpace);
 
 	// Draw the camera puck transform
 	const glm::mat4 cameraPuckXform = glm::dmat4(m_cameraTrackingPuckView->getCalibrationPose());
@@ -296,9 +299,9 @@ void MonoLensTrackerPoseCalibrator::renderVRSpaceCalibrationState()
 	const float zNear= fmaxf(m_calibrationState->inputCameraIntrinsics.znear, 0.1f);
 	const float zFar = fminf(m_calibrationState->inputCameraIntrinsics.zfar, 2.0f);
 	drawTransformedFrustum(
-		m_calibrationState->cameraXform,
+		m_calibrationState->cameraXform_VRSpace,
 		hfov_radians, vfov_radians,
 		zNear, zFar,
 		Colors::Yellow);
-	drawTransformedAxes(m_calibrationState->cameraXform, 0.1f);
+	drawTransformedAxes(m_calibrationState->cameraXform_VRSpace, 0.1f);
 }
