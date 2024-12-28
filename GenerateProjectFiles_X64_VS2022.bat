d87d49b7b06b8945d0de23577c71b8047276af0c
Brendan Walker <brendan@millerwalker.net>
2024-12-28 15:36:09 -0800

    Fix RML install path

--- GenerateProjectFiles_X64_VS2022.bat
+++ GenerateProjectFiles_X64_VS2022.bat
@@ -5,9 +5,11 @@ pushd build
 
 echo "Rebuilding Mikan x64 Project files..."
 set DEPS_ROOT_PATH=%~dp0/deps
-set THIRDPARTY_ROOT_PATH=%~dp0\thirdparty
+set THIRDPARTY_ROOT_PATH=%~dp0/thirdparty
+set DIST_ROOT_PATH=%~dp0/dist/Win64
 
 cmake .. -G "Visual Studio 17 2022" -A x64 ^
+-DCMAKE_INSTALL_PREFIX=%DIST_ROOT_PATH% ^
 -DOpenCV_DIR=%DEPS_ROOT_PATH%\opencv\build ^
 -DOPENVR_ROOT_DIR=%THIRDPARTY_ROOT_PATH%\openvr ^
 -DOPENVR_HEADERS_ROOT_DIR=%THIRDPARTY_ROOT_PATH%openvr\include ^
