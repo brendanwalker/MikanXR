7b38a533cb99cc6d3f5725882df4d940392c618e
Brendan Walker <brendan@millerwalker.net>
2025-02-05 22:36:23 -0800

    Move text and line renderer back into editor

--- src/Editor/Renderer/MikanRendererFwd.h
+++ src/Editor/Renderer/MikanRendererFwd.h
@@ -5,3 +5,17 @@
 
 class GlRmlUiRender;
 using GlRmlUiRenderUniquePtr = std::unique_ptr<GlRmlUiRender>;
+
+class GlCamera;
+using GlCameraPtr = std::shared_ptr<GlCamera>;
+using GlCameraConstPtr = std::shared_ptr<const GlCamera>;
+
+class GlScene;
+using GlScenePtr = std::shared_ptr<GlScene>;
+using GlSceneWeakPtr = std::weak_ptr<GlScene>;
+using GlSceneConstPtr = std::shared_ptr<const GlScene>;
+
+class GlViewport;
+using GlViewportPtr = std::shared_ptr<GlViewport>;
+using GlViewportWeakPtr = std::weak_ptr<GlViewport>;
+using GlViewportConstPtr = std::shared_ptr<const GlViewport>;
