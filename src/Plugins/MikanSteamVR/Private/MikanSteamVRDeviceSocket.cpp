c585324c05f94dd58c9031c6f1783308d5ceb521
brendanwalker <brendan@millerwalker.net>
2026-04-26 00:32:36 -0700

    Fixed incorrect parameters to vr::IVRRenderModels::GetComponentState()

--- src/Plugins/MikanSteamVR/Private/MikanSteamVRDeviceSocket.cpp
+++ src/Plugins/MikanSteamVR/Private/MikanSteamVRDeviceSocket.cpp
@@ -25,19 +25,19 @@ bool MikanSteamVRDeviceSocket::getSocketState(VRDevicePose& outRelativePose) con
 		m_ownerDevice->getProperties()->getRenderModelName();
 
 	vr::VRControllerState_t* controllerState= nullptr;
-	vr::RenderModel_ComponentState_t* componentState= nullptr;
-	vr::RenderModel_ControllerMode_State_t* componentModeState= nullptr;
+	vr::RenderModel_ComponentState_t componentState= {};
+	vr::RenderModel_ControllerMode_State_t componentModeState= {};
 
 	if (renderModelInterface != nullptr &&
 		renderModelInterface->GetComponentState(
 			ownerRenderModelName.c_str(),
 			m_socketName.c_str(),
 			controllerState,
-			componentModeState,
-			componentState))
+			&componentModeState,
+			&componentState))
 	{
 		const glm::mat4 componentToRenderMat =
-			vr_HmdMatrix34_to_glm_mat4(componentState->mTrackingToComponentLocal);
+			vr_HmdMatrix34_to_glm_mat4(componentState.mTrackingToComponentLocal);
 
 		outRelativePose = glm_mat4_to_VRDevicePose(componentToRenderMat);
 		return true;
