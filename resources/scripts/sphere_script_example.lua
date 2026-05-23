f223b4cd5ada2dc4bea5c607d6771719a7611935
brendanwalker <brendan@millerwalker.net>
2026-05-23 01:08:05 -0700

    Cleaning up lua test scripts

--- resources/scripts/sphere_script_example.lua
+++ resources/scripts/sphere_script_example.lua
@@ -1,21 +1,25 @@
 local easing = require("resources/scripts/easing")
 
 local function sphere_scale_bounce_coroutine()
-  local model_stencil_list= ModelStencilList()
-  local sphere_stencil= ModelStencil(model_stencil_list:getStencilId(3))
-  local final_scale= Vec3f(10.0, 10.0, 10.0)
+  local sphere_stencil = ModelStencilSystem:getModelStencilByName("Sphere")
+  if sphere_stencil == nil then
+    print("sphere_script_example: could not find ModelStencil named 'Sphere'")
+    return
+  end
+
+  local final_scale = Vec3f(10.0, 10.0, 10.0)
   local elapsed_time = 0
   local duration = 1.5
-  
+
   while (elapsed_time < duration)
   do
-    local start= 0
-    local change= 1 - start
+    local start = 0
+    local change = 1 - start
     local s = easing.outBounce(elapsed_time, start, change, duration)
     local current_scale = final_scale:scaleUniform(s)
-    sphere_stencil:setModelScale(current_scale)
+    sphere_stencil.relativeScale = current_scale
     wait_next_frame()
-    elapsed_time= elapsed_time + get_frame_delta_seconds()
+    elapsed_time = elapsed_time + get_frame_delta_seconds()
   end
 end
 
@@ -25,11 +29,13 @@ function play_stencil_scale_bounce()
 end
 
 function reset_stencil_scale()
-    local model_stencil_list= ModelStencilList()
-    local sphere_stencil= ModelStencil(model_stencil_list:getStencilId(3))
-    local reset_scale= Vec3f(0.0, 0.0, 0.0)
-    sphere_stencil:setModelScale(reset_scale)
-    ScriptContext.broadcastMessage("resetCameraZoom")
+  local sphere_stencil = ModelStencilSystem:getModelStencilByName("Sphere")
+  if sphere_stencil == nil then
+    print("sphere_script_example: could not find ModelStencil named 'Sphere'")
+    return
+  end
+  sphere_stencil.relativeScale = Vec3f(0.0, 0.0, 0.0)
+  ScriptContext.broadcastMessage("resetCameraZoom")
 end
 
 ScriptContext.registerTrigger("play_stencil_scale_bounce")
