7abf05f4b1ac95cce666b1b41d29fa6581241c73
brendanwalker <brendan@millerwalker.net>
2026-08-22 02:27:28 -0700

    Exporting missing light_environment_id in client api.

--- bindings/typescript/types/MikanCameraTypes.ts
+++ bindings/typescript/types/MikanCameraTypes.ts
@@ -8,6 +8,7 @@ export class MikanCameraComponentValues extends MikanTransformComponentValues {
   stage_id: number = -1;
   tracking_mount_id: number = -1;
   video_source_id: number = -1;
+  light_environment_id: number = -1;
   tracking_frame_delay: number = 0;
   aperture_orientation_offset: MikanQuatd = new MikanQuatd();
   aperture_position_offset: MikanVector3d = new MikanVector3d();
@@ -17,6 +18,7 @@ export class MikanCameraComponentValues extends MikanTransformComponentValues {
     { name: 'stage_id', type: 'int32' },
     { name: 'tracking_mount_id', type: 'int32' },
     { name: 'video_source_id', type: 'int32' },
+    { name: 'light_environment_id', type: 'int32' },
     { name: 'tracking_frame_delay', type: 'int32' },
     { name: 'aperture_orientation_offset', type: 'MikanQuatd' },
     { name: 'aperture_position_offset', type: 'MikanVector3d' },
